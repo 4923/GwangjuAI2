6221a00c5e19472c0bc955aa1d882bb5850af68b
4923 <www_1216@hufs.ac.kr>
2021-06-30 10:19:27 +0900

    Style : solution -> solve

--- 02_Rotation/Algorithmn/Practice/equal_quotient_remainder/main.py
+++ 02_Rotation/Algorithmn/Practice/equal_quotient_remainder/main.py
@@ -11,20 +11,20 @@ if Quotient = Remainder = X
 """
 
 # input
-def solution1(N=int(input())):
+def solve1(N=int(input())):
     answer = 0
     for X in range(N - 1, 0, -1):  # 자기자신으로 나누면 나머지가 0이므로 범위에서 제외
         answer += (N + 1) * X  # 공식에 따라 N+1에 변화하는 값 X를 곱하여 총계에 합산
     return answer
 
 
-print(solution1())
+print(solve1())
 
 # # solution 2 : if condition
 # 최대값인 N * N -1 까지 돌면서 N - 1 까지의 수로 나눈 후 나머지와 몫 비교
 
 
-def solution2(N=int(input())):
+def solve2(N=int(input())):
     answer = 0
     for num in range(1, N * N):
         if num // N == num % N:
@@ -32,4 +32,4 @@ def solution2(N=int(input())):
     return answer
 
 
-print(solution2())
+print(solve2())
