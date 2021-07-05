518f2e24d2cfacf5278a81ca93bc7777cd2a6520
4923 <www_1216@hufs.ac.kr>
2021-07-05 12:52:32 +0900

    Update : apply bootstrap_1 css layout

--- 02_Rotation/Algorithmn/Practice/max_min/main.py
+++ 02_Rotation/Algorithmn/Practice/max_min/main.py
@@ -1,5 +1,6 @@
 # 최대값, 최소값 구하기
 
+# solution 1
 def max_min(numbers = list(map(int, input().split()))):
     MAX, MIN = numbers[0], numbers[0]
     for number in numbers:
@@ -8,3 +9,19 @@ def max_min(numbers = list(map(int, input().split()))):
     return (MAX, MIN)
 
 print(max_min())
+
+# solution 2 : halving
+
+# find_max
+def find_max(numbers):
+    MAX = numbers[0]
+    for number in numbers:
+        if number > MAX: MAX = number
+    return MAX 
+
+def halving(numbers):
+    leng = len(numbers)
+    # MAX_1, MAX_2 = find_max(numbers[:leng//2]), find_max(numbers[leng//2:])
+    # eh.......
+
+
