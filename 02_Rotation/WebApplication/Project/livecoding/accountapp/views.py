d6454a46f092087439f4581b72f78af8829fa2f6
4923 <www_1216@hufs.ac.kr>
2021-07-12 11:14:35 +0900

    Feature : input tag which returns user input
    - views.py : POST.get, context
    - hello_world.html

--- 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
@@ -5,10 +5,8 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):  # register -> request
     if request.method == "POST":
-        return render(
-            request, "accountapp/hello_world.html", context={"text": "POST METHOD!"}
-        )
+        # temp : user에게 받은 입력을 받았다가 되돌려준다 (return에서)
+        temp = request.POST.get("input_text")  # post 방식에서 get을 사용하면 post 안의 데이터를 뽑아낼 수 있다.
+        return render(request, "accountapp/hello_world.html", context={"text": temp})
     else:  # GET
-        return render(
-            request, "accountapp/hello_world.html", context={"text": "GET METHOD!"}
-        )
+        return render(request, "accountapp/hello_world.html", context={"text": "GET METHOD!"})
