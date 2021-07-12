279ed5a702686d09cfdeb182a64734153213b5d6
4923 <www_1216@hufs.ac.kr>
2021-07-12 12:07:17 +0900

    Feature : connect db, print db object all
    - djangoguide/08-05

--- 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
@@ -1,10 +1,25 @@
 # from django.http import HttpResponse
+from accountapp.models import HelloWorld
 from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get("input_text")
-        return render(request, "accountapp/hello_world.html", context={"text": temp})
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(
+            request,
+            "accountapp/hello_world.html",
+            context={"hello_world_list": hello_world_list},
+        )
     else:
-        return render(request, "accountapp/hello_world.html", context={"text": "GET METHOD!"})
+        hello_world_list = HelloWorld.objects.all()
+        return render(
+            request, "accountapp/hello_world.html", context={"hello_world_list": hello_world_list}
+        )
