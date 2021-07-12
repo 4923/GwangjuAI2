c52ad23b6b9d5ea0126b0002e6f5452b0e7c8b55
4923 <www_1216@hufs.ac.kr>
2021-07-12 14:38:47 +0900

    Feature : return items in DB
    - new_hello_world
    - djangoguide/08-05

--- 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
@@ -1,12 +1,25 @@
-# from django.http import HttpResponse, request
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):  # register -> request
     if request.method == "POST":
         # temp : user에게 받은 입력을 받았다가 되돌려준다 (return에서)
         temp = request.POST.get("input_text")  # post 방식에서 get을 사용하면 post 안의 데이터를 뽑아낼 수 있다.
-        return render(request, "accountapp/hello_world.html", context={"text": temp})
-    else:  # GET
-        return render(request, "accountapp/hello_world.html", context={"text": "GET METHOD!"})
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
+
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(
+            request, "accountapp/hello_world.html", context={"hello_world_list": hello_world_list}
+        )
