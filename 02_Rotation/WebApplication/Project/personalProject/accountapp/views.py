c717dcb787194e0f99bbfb04e480b92df3e54b91
4923 <www_1216@hufs.ac.kr>
2021-07-12 12:48:02 +0900

    Add input bootstrap

--- 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
@@ -7,15 +7,15 @@ from django.urls import reverse
 # Create your views here.
 def hello_world(request):
     if request.method == "POST":
+
         temp = request.POST.get("input_text")
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
-
         return HttpResponseRedirect(reverse("accountapp:hello_world"))
+
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(
