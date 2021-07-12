f284c3b26ee429735cfc02b8a92c440821c5f6e4
4923 <www_1216@hufs.ac.kr>
2021-07-12 12:30:08 +0900

    Feature : redirect
    - change POST type to GET

--- 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/personalProject/accountapp/views.py
@@ -1,6 +1,8 @@
-# from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
 from django.shortcuts import render
+
+from django.urls import reverse
 
 # Create your views here.
 def hello_world(request):
@@ -13,11 +15,7 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
 
-        return render(
-            request,
-            "accountapp/hello_world.html",
-            context={"hello_world_list": hello_world_list},
-        )
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(
