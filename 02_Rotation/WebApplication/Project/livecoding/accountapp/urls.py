e31afa910d4de3151d40e206f03268715d8534c1
4923 <www_1216@hufs.ac.kr>
2021-07-13 18:13:30 +0900

    Feature : login page

--- 02_Rotation/WebApplication/Project/livecoding/accountapp/urls.py
+++ 02_Rotation/WebApplication/Project/livecoding/accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import AccountCreateView, hello_world
@@ -7,6 +8,8 @@ app_name = "accountapp"
 # 어떤 주소를 적어야 들어갈 수 있는지
 urlpatterns = [
     path("hello_world/", hello_world, name="hello_world"),
+    path("login/", LoginView.as_view(template_name="accountapp/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("create/", AccountCreateView.as_view(), name="create"),
     # 두번째 인자로 가져와야 할 건 함수 (view) 인데 AccountCreateView는 클래스라 .as_view를 추가해야 한다. 추가하면 view로 가져올 수 있음.
 ]
