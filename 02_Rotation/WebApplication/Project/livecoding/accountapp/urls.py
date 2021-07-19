29fce4f7793a1c7a8618a3c6e777ce9b56cddd4a
4923 <www_1216@hufs.ac.kr>
2021-07-19 11:50:42 +0900

    Feature : Update info

--- 02_Rotation/WebApplication/Project/livecoding/accountapp/urls.py
+++ 02_Rotation/WebApplication/Project/livecoding/accountapp/urls.py
@@ -1,19 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import AccountCreateView, hello_world, AccountDetailView
+from accountapp.views import AccountCreateView, hello_world, AccountDetailView, AccountUpdateView
 
 app_name = "accountapp"
 
 # 어떤 주소를 적어야 들어갈 수 있는지
 urlpatterns = [
     path("hello_world/", hello_world, name="hello_world"),
-    path(
-        "login/", LoginView.as_view(template_name="accountapp/login.html"), name="login"
-    ),
+    path("login/", LoginView.as_view(template_name="accountapp/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("create/", AccountCreateView.as_view(), name="create"),
     # 두번째 인자로 가져와야 할 건 함수 (view) 인데 AccountCreateView는 클래스라 .as_view를 추가해야 한다. 추가하면 view로 가져올 수 있음.
     path("create/", AccountCreateView.as_view(), name="create"),
     path("detail/<int:pk>", AccountDetailView.as_view(), name="detail"),
+    path("update/<int:pk>", AccountUpdateView.as_view(), name="update"),  # route 이름이 update
 ]
--- 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import HelloWorld
 
@@ -13,9 +13,7 @@ from accountapp.models import HelloWorld
 def hello_world(request):  # register -> request
     if request.method == "POST":
         # temp : user에게 받은 입력을 받았다가 되돌려준다 (return에서)
-        temp = request.POST.get(
-            "input_text"
-        )  # post 방식에서 get을 사용하면 post 안의 데이터를 뽑아낼 수 있다.
+        temp = request.POST.get("input_text")  # post 방식에서 get을 사용하면 post 안의 데이터를 뽑아낼 수 있다.
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
@@ -47,3 +45,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = "target_user"  # 이 이름을 통해 html에서 account 객체에 접근하게 된다 (!!)
     template_name = "accountapp/detail.html"
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = "target_user"
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/update.html"
