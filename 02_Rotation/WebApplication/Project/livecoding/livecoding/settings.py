11a8848a73aa19a4447cf95cd3010be328031c1a
4923 <www_1216@hufs.ac.kr>
2021-07-22 12:45:54 +0900

    Feature : new app 'profileapp'
    - path : ~/profileapp

--- 02_Rotation/WebApplication/Project/livecoding/livecoding/settings.py
+++ 02_Rotation/WebApplication/Project/livecoding/livecoding/settings.py
@@ -58,8 +58,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accountapp",
     "bootstrap4",
+    "accountapp",
+    "profileapp",
 ]
 
 MIDDLEWARE = [
