8c816c9d7a9a334614f6b1824745325431b8eb86
4923 <www_1216@hufs.ac.kr>
2021-07-26 12:35:58 +0900

    Feature : create template
    - connect url
    - create template
    - feature logic : ProfileCreateView

--- 02_Rotation/WebApplication/Project/personalProject/profileapp/urls.py
+++ 02_Rotation/WebApplication/Project/personalProject/profileapp/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from profileapp.views import ProfileCreationView
+
+app_name = "profileapp"
+
+urlpatterns = [path("create/", ProfileCreationView.as_view(), name="create")]
