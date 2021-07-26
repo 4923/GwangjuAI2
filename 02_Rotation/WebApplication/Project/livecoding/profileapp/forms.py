c76778688b80adcf42618af767c7452346dc1fce
4923 <www_1216@hufs.ac.kr>
2021-07-26 12:38:09 +0900

    Feature : create template
    - connect url
    - create template
    - feature logic : ProfileCreateView

--- 02_Rotation/WebApplication/Project/livecoding/profileapp/forms.py
+++ 02_Rotation/WebApplication/Project/livecoding/profileapp/forms.py
@@ -10,4 +10,4 @@ class ProfileCreationForm(ModelForm):
         # 서버는 클라이언트를 믿으면 안되기 때문 : user는 제외한다. (내것만 만들도록 설정)
         # 서버 내에서 직접 요청을 보내는 사람이 누구인지 확인하고 그 유저 객체의 프로필을 서버가 배정하도록 함.
 
-        fields = ["image", "nickname", "message"]
+        fields = ["image", "nickname", "messages"]
--- 02_Rotation/WebApplication/Project/livecoding/profileapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/profileapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
 # Create your views here.
+class ProfileCreationView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    # 완성되었을 때 향할 곳
+    success_url = reverse_lazy("accountapp:hello_world")
+    # 프로필을 만들 때 어떤 페이지를 기준으로 렌더링 할지
+    template_name = "profileapp/create.html"
