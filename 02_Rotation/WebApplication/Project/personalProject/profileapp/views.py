59b12f21bd2910702816b5ae1158ff691653f17f
4923 <www_1216@hufs.ac.kr>
2021-07-29 10:46:34 +0900

    Fix : wrong View class
    - modify creation
    - Add update

--- 02_Rotation/WebApplication/Project/personalProject/profileapp/views.py
+++ 02_Rotation/WebApplication/Project/personalProject/profileapp/views.py
@@ -3,12 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
 class ProfileCreationView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm  # form에서 import
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "profileapp/create.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     success_url = reverse_lazy("accountapp:hello_world")
