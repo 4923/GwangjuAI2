45aee5ec82414eb03023b37e2e667eddcb8934ad
4923 <www_1216@hufs.ac.kr>
2021-08-02 11:48:46 +0900

    Fix : profile image not visible
    - projectapp/urls.py에서 static 추가 하지 않아서 발생하는 문제
    - 엑박 문제 해결

--- 02_Rotation/WebApplication/Project/personalProject/personalProject/urls.py
+++ 02_Rotation/WebApplication/Project/personalProject/personalProject/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accountapp.urls")),
     path("profiles/", include("profileapp.urls")),
     path("articles/", include("articleapp.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
