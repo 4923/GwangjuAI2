0492f2eb4672523ac3e4a7d20736e9f22b69d94c
4923 <www_1216@hufs.ac.kr>
2021-08-03 17:03:32 +0900

    Feature : post article process

--- 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
@@ -1,3 +1,4 @@
+from articleapp.views import ArticleCreateView
 from django.views.generic import TemplateView
 from django.urls import path
 
@@ -5,4 +6,5 @@ app_name = "articleapp"
 
 urlpatterns = [
     path("list/", TemplateView.as_view(template_name="articleapp/list.html"), name="list"),
+    path("create/", ArticleCreateView.as_view(), name="create"),
 ]
--- 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreateForm
+from articleapp.models import Article
 
 # Create your views here.
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreateForm  # model form을 기반으로 만들었다.
+    success_url = reverse_lazy("articleapp:list")
+    template_name = "articleapp/create.html"
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
