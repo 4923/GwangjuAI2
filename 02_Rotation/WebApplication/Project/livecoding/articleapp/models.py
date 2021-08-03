215d185d5e4341d64634df468866a8ea64e251b6
4923 <www_1216@hufs.ac.kr>
2021-08-03 16:47:24 +0900

    Fix : typo class name

--- 02_Rotation/WebApplication/Project/livecoding/articleapp/models.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Profile(models.Model):
+class Article(models.Model):
     # 작성자 정보
     writer = models.OneToOneField(
         User, on_delete=models.SET_NULL, related_name="article", null=True
