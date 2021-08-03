009e81512d1acf022c020a3b1f168dcc8fd64f07
4923 <www_1216@hufs.ac.kr>
2021-08-03 17:18:20 +0900

    Feature : Detail View

--- 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
@@ -1,4 +1,4 @@
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 from django.views.generic import TemplateView
 from django.urls import path
 
@@ -7,4 +7,5 @@ app_name = "articleapp"
 urlpatterns = [
     path("list/", TemplateView.as_view(template_name="articleapp/list.html"), name="list"),
     path("create/", ArticleCreateView.as_view(), name="create"),
+    path("detail/<int:pk>", ArticleDetailView.as_view(), name="detail"),
 ]
--- 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 
 from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
 
 from articleapp.forms import ArticleCreateForm
 from articleapp.models import Article
@@ -17,3 +18,9 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = "target_article"  # 객체 지정
+    template_name = "articleapp/detail.html"  # 렌더링 페이지
