7b326fbba4c85aa0727cc7141465911bb6cb5e43
4923 <www_1216@hufs.ac.kr>
2021-08-03 17:57:02 +0900

    Feature : UpdateView

--- 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/urls.py
@@ -1,4 +1,4 @@
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 from django.views.generic import TemplateView
 from django.urls import path
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("list/", TemplateView.as_view(template_name="articleapp/list.html"), name="list"),
     path("create/", ArticleCreateView.as_view(), name="create"),
     path("detail/<int:pk>", ArticleDetailView.as_view(), name="detail"),
+    path("update/<int:pk>", ArticleUpdateView.as_view(), name="update"),
 ]
--- 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
+++ 02_Rotation/WebApplication/Project/livecoding/articleapp/views.py
@@ -1,7 +1,8 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 
 from articleapp.forms import ArticleCreateForm
 from articleapp.models import Article
@@ -24,3 +25,16 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = "target_article"  # 객체 지정
     template_name = "articleapp/detail.html"  # 렌더링 페이지
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreateForm
+    context_object_name = "target_article"
+    template_name = "articleapp/update.html"
+
+    def get_success_url(self):
+        return reverse("articleapp:detail", kwargs={"pk": self.object.pk})
+        # self.object == target_article (거의 동일)
+        # Article은 user를 가지고있지 않으니 self.object.user.pk는 불가 / 대신 self.object.writer.pk는 가능
+        # 식별하기 번거로우므로 self.object.pk로 지정
