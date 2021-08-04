afba0d657d3e2bf95a8de030415ba275a06e08c1
4923 <www_1216@hufs.ac.kr>
2021-08-04 22:14:30 +0900

    Resolve : BOJ 2164 (close #2)

--- 02_Rotation/Algorithm/Practice/queue/card2.py
+++ 02_Rotation/Algorithm/Practice/queue/card2.py
@@ -1,14 +1,6 @@
 from collections import deque
 
 
-def card(N, Q):
-    for _ in range(N - 1):
-        remove = Q.popleft()  # 맨 위의 카드는 제거
-        back = Q.popleft()
-        Q.append(back)
-    return Q[0]
-
-
 def card2(N, Q):
     for _ in range(N - 1):
         Q.popleft()  # 맨 위의 카드는 제거
@@ -17,10 +9,13 @@ def card2(N, Q):
 
 
 def main():
-    N = int(input("Input N : "))
+    # N = int(input("Input N : "))      # BOJ 오답처리
+    N = int(input())  # BOJ 정답
     Q = deque([num + 1 for num in range(N)])
-    last_card = card(N, Q)
+    last_card = card2(N, Q)
     print(last_card)
+
+    print(type(last_card))
 
 
 if __name__ == "__main__":
