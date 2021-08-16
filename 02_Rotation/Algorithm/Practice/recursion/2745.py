4348acb74125f44be2ebca185a09cd02f507299a
4923 <www_1216@hufs.ac.kr>
2021-08-16 17:10:41 +0900

    Resolve #6 : recursion try2 resolve (index)
    - '123'[0] != 3, '123'[0] == 1

--- 02_Rotation/Algorithm/Practice/recursion/2745.py
+++ 02_Rotation/Algorithm/Practice/recursion/2745.py
@@ -9,40 +9,43 @@
 # 출력
 # 첫째 줄에 B진법 수 N을 10진법으로 출력한다.
 
-# N, B = map(str, input().split())
+N, B = map(str, input().split())
 
-# result = 0
-# for idx, digit in enumerate(N):
-#     toDecimal = int(B) ** idx
-#     if not digit.isdigit():
-#         digit = ord(digit) - 55
-#     print(digit, type(digit))
-#     result += toDecimal * digit
+result = 0
+for idx, digit in enumerate(N):
+    toDecimal = int(B) ** idx
+    if not digit.isdigit():
+        digit = ord(digit) - 55
+    print(digit, type(digit))
+    result += toDecimal * digit
 
-# print(result)
+print(result)
 
 # try 1 : isdigit, isnumeric 모두 같은 문제 발생
 # TypeError(연산, 함수가 계산할 때 데이터의 유형이 잘못되었을 때)
+# N이 str인 상황에서 loop 돌렸기 때문인듯
 
 # ------------------------ #
 
 
-def to10(N, B, index, result):
-    if index == len(N):
+def to10(list_N, B, index, result):
+    if len(list_N) == 0:
         print(result)
         return result
 
-    decimal = N[index]
-    if not decimal.isdigit():
-        decimal = ord(decimal) - 55
-    result += int(decimal) * int(B) ** index
-    to10(N, B, index + 1, result)
+    digit = list_N.pop()
+    if not digit.isdigit():
+        digit = ord(digit) - 55
+    result += int(digit) * int(B) ** index
+    # print(f"(digit {digit}) * (index {index}) ** (B {B}) =  {result} ")
+
+    index += 1
+    to10(list_N, B, index, result)
 
 
 def main():
     N, B = map(str, input().split())
-    index, result = 0, 0
-    to10(N, B, index, result)
+    to10(list(N), B, index=0, result=0)
 
 
 if __name__ == "__main__":
@@ -51,3 +54,8 @@ if __name__ == "__main__":
 
 # try 2 : 재귀로 풀었을 때 입력값 10 10 에서 정답 1 출력되는 문제
 # 10이라는 값이 10진법일때 이를 10진법으로 변환하면 10이 출력되어야 함.
+# (digit 1) * (index 0) * (B 10) =  result : 1
+# (digit 0) * (index 1) * (B 10) =  result : 1
+# 1
+# digit이 1 -> 0 으로 잡히기 때문. 왼쪽부터 시작이니 당연히 왼쪽부터 십의자리수가 잡힌다.
+# -> RESOLVE
